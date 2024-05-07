5e2569a1183f2cf3b2a3163c6d7a4a46faf8ee90
Cat4848 <catalinrol@gmail.com>
2024-05-07 16:44:04 +0100

    add: makeTree recursion

--- playground/recursion.js
+++ playground/recursion.js
@@ -3,3 +3,22 @@ export function countdown(n) {
   countdown(n - 1);
 }
 countdown(4);
+
+const categories = [
+  { id: "animals", parent: null },
+  { id: "mammals", parent: "animals" },
+  { id: "cats", parent: "mammals" },
+  { id: "dogs", parent: "mammals" },
+  { id: "chihuahua", parent: "dogs" },
+  { id: "labrador", parent: "dogs" },
+  { id: "persian", parent: "cats" },
+  { id: "siamese", parent: "cats" }
+];
+
+export function makeTree(categories, parent) {
+  let tree = {};
+  const filteredCategories = categories.filter((c) => c.parent === parent);
+  filteredCategories.forEach((c) => (tree[c.id] = makeTree(categories, c.id)));
+  return tree;
+}
+const tree = makeTree(categories, null);
