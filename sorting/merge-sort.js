a6886dbac28505017d842ed94fedf9d92b146dc7
Cat4848 <catalinrol@gmail.com>
2024-05-07 09:18:33 +0100

    add: merge function part of merge sort algorithm

--- sorting/merge-sort.js
+++ sorting/merge-sort.js
@@ -1,3 +1,46 @@
-function mergeSort() {
-  
+// part of the divide and conquer algorithms
+
+function mergeSort() {}
+
+function merge(list, p, q, r) {
+  const leftLength = q - p;
+  const rightLength = r - q;
+
+  const leftList = [];
+  const rightList = [];
+
+  for (let i = 0; i < leftLength; i++) {
+    leftList[i] = list[p + i];
+  }
+
+  for (let j = 0; j < rightLength; j++) {
+    rightList[j] = list[q + j + 1];
+  }
+
+  let i = 0;
+  let j = 0;
+  let k = p;
+
+  while (i < leftLength && j < rightLength) {
+    if (leftList[i] <= rightList[j]) {
+      list[k] = leftList[i];
+      i++;
+    } else {
+      list[k] = rightList[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < leftLength) {
+    list[k] = leftList[i];
+    i++;
+    k++;
+  }
+
+  while (j < rightLength) {
+    list[k] = rightList[j];
+    j++;
+    k++;
+  }
 }
