8e24a1a1caf466a006483cae08df83ff441923d0
Cat4848 <catalinrol@gmail.com>
2025-07-30 07:52:59 +0100

    refactor: improved folder structure

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import binarySearch from "./searching/binarySearch/binarySearch.js";
+import binarySearch from "./src/binarySearch/binarySearch.js";
 
 const num = [2, 4, 6, 7, 8, 10, 12, 14, 15, 17, 20, 21, 23];
 const v = 17;
